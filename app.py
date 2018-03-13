b765fc38a8ca8fbca7d669bf0f4463c1d9e80373
Joe Wang <joewang704@gmail.com>
2018-03-13 17:38:32 -0400

    Add db connection test endpoint

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 import os
 
 app = Flask(__name__)
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'poloisbae'
+app.config['MYSQL_DB'] = 'db'
+mysql = MySQL(app)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/testDbConn')
+def testDbConn():
+    cur = mysql.connection.cursor()
+    return 'success!'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
