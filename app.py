cfa525e5b9e19a9c943a0c7bc5ab232dbb17c7d7
Yamilex Avila-Stanley <yavila.stanley@gmail.com>
2018-04-03 17:14:13 -0400

    add dummy endpoint for recommendation

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_mysqldb import MySQL
+from flask import jsonify
+
 import os
 
 app = Flask(__name__)
@@ -16,6 +18,18 @@ def home():
 def testDbConn():
     cur = mysql.connection.cursor()
     return 'success!'
+
+@app.route('/recommendation')
+def recommendation():
+	cur = mysql.connection.cursor()
+	query = "select business.id, name from business join category on business.id = category.business_id where state = 'AZ' and category.category='Restaurants' limit 10"
+	cur.execute(query)
+	data = cur.fetchall()
+	data = map(lambda x: { "id": x[0], "restaurant": x[1] }, data)
+	print(data)
+	return jsonify(data)
+
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
