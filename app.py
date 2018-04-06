b3836fbb8c2dc52ba6574631f6cf3ef1bd7ec9bf
Joe Wang <joewang704@gmail.com>
2018-04-06 17:13:07 -0400

    Create working force directed graph

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'poloisbae'
-app.config['MYSQL_DB'] = 'db'
+app.config['MYSQL_DB'] = 'yelp_db'
 mysql = MySQL(app)
 
 @app.route('/')
@@ -22,12 +22,11 @@ def testDbConn():
 @app.route('/recommendation')
 def recommendation():
 	cur = mysql.connection.cursor()
-	query = "select business.id, name from business join category on business.id = category.business_id where state = 'AZ' and category.category='Restaurants' limit 10"
+	query = "select business.id, name from business join category on business.id = category.business_id where state = 'AZ' and category.category='Restaurants' order by rand() limit 10"
 	cur.execute(query)
 	data = cur.fetchall()
 	data = map(lambda x: { "id": x[0], "restaurant": x[1] }, data)
 	return jsonify(data)
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
